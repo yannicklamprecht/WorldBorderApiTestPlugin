b58f44c96177b15f83684e2a3103406ea3076d2a
Yannick Lamprecht <yannicklamprecht@live.de>
2021-08-18 00:58:04 +0200

    separate worldbordertestplugin

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    // Apply the application plugin to add support for building a CLI application in Java.
+    java
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+description = "testplugin"
+version = "1.170.0"
+
+tasks {
+    processResources {
+        from(sourceSets.main.get().resources.srcDirs) {
+            filesMatching("plugin.yml") {
+                expand(
+                    "version" to project.version
+                )
+            }
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
+    }
+}
+
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven {
+        name = "eldonexus"
+        // url = uri("https://eldonexus.de/repository/maven-snapshots/")
+        url = uri("https://eldonexus.de/repository/maven-releases/")
+    }
+    maven { url = uri("https://repo.aikar.co/content/groups/aikar/") }
+}
+
+dependencies {
+    implementation("co.aikar:acf-bukkit:0.5.0-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    compileOnly("com.github.yannicklamprecht.worldborderapi:api:1.170.0")
+}
